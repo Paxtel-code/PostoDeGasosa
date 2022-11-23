35e0814862be233b4598c730b202e09bf92a136c
Eduardo Fadel <1eduardofadel@gmail.com>
2022-11-23 11:47:46 -0300

    Entrega 3

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
+#include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 
 void flush_in() {
     int ch;
@@ -45,11 +47,11 @@ void relatorio() {
 }
 
 int menu(int op) {
-    printf("\n[!]Digite a opcao desejada[!]\n"
+    printf("Digite a opção desejada:\n"
            "1 - Adicionar carro\n"
-           "2 - Abastecer proximo carro\n"
+           "2 - Abastecer próximo carro\n"
            "3 - Exibir fila de espera\n"
-           "4 - Relatorio geral\n"
+           "4 - Relatório geral\n"
            "5 - Encerrar\n");
     scanf("%d", &op);
     return op;
@@ -60,42 +62,48 @@ int main(void) {
     float valLitro;
     int tamFila;
     int op;
+    int atendidos;
     int bomba = 200;
+    int caixa;
+    int litros;
+    char vAtendidos[100][255];
 
     // introducao ------------------------------------
     printf("Autor: Eduardo Fadel\n");
     printf("..:: Posto de Gasolina ::..\n");
-    printf("O programa e um simulador de posto de gasolina que possui apenas uma "
-           "bomba, o algoritmo e efetuado por meio de uma fila de carros que sao "
-           "abastecidos por uma quantidade determinada de gasolina\n");
+    printf("O programa é um simulador de posto de gasolina que possui apenas uma "
+           "bomba, o algoritmo é efetuado por meio de uma fila de carros que sao "
+           "abastecidos por uma quantidade determinada de gasolina\n\n");
     //------------------------------------------------
 
-    // requisitos iniciais ---------------------------
-    printf("Digite o preco da gasosa:\n");
+    // requisitos iniciais ----------------------------
+    printf("Digite o preço da gasosa\n");
     scanf("%f", &valLitro);
-    printf("Digite o tamanho da fila:\n");
+    printf("Digite o tamanho da fila\n");
     scanf("%d", &tamFila);
+    char carros[tamFila][255];
     //------------------------------------------------
 
     while (op != 5) {
-
         op = menu(op);
-
         switch (op) {
             case 1: // adicionar carro
-                printf("\nCarro adicionado\n");
+
+                printf("Carro adicionado\n");
                 break;
             case 2: // abastecer
-                printf("\nCarro abastecido, proximo da fila: \n");
+                printf("Carro abastecido, proxímo da fila\n");
+
                 break;
             case 3: // exibir fila
-                printf("\nFila:\n");
+                printf("Fila:\n");
                 break;
             case 4: // relatorio
+                printf("Relatório:\n");
                 relatorio();
                 break;
             case 5: // encerrar
-                printf("\nAdeus\n");
+                printf("Adeus\n");
                 break;
         }
     }
